1c6cbfdc08ecf6d8f7b543b2623044aaf776b553
Nguyễn Khánh <khanh94@MacBook-Air-cua-Nguyen.local>
2021-07-05 18:37:08 +0700

    new_script

--- Biomarker_metaanalysis.R
+++ Biomarker_metaanalysis.R
@@ -3,7 +3,7 @@ library(metafor)
 library(readxl)
 library(dplyr)
 
-setwd('/Users/khanh94/Downloads')
+setwd('/Users/khanh94/Documents/MetaAnalysis')
 
 #Read the data
 data <- read_excel('Complete_Data.xlsx')
@@ -30,16 +30,16 @@ data2 <- data %>%
   mutate_at('Number of Markers', as.numeric)
 
 #Convert all string columns to factors
-data2_factor <- data2 %>% 
+data2_CA19 <- data2 %>% 
   mutate_if(sapply(data2, is.character), as.factor)
 
-#Generate the metaanalysis
-m.gen_results <- metagen(TE = data2_factor$AUC,
-                     seTE = data2_factor$"Calculated SE",
+#Generate the metaanalysis for the markers containing CA19-9 obly
+m.gen_results <- metagen(TE = data2_CA19$AUC,
+                     seTE = data2_CA19$"Calculated SE",
                      lower = Q1,
                      upper = Q2,
-                     data = data2_factor,
-                     studlab = data2_factor$"Study ID",
+                     data = data2_CA19,
+                     studlab = data2_CA19$"Study ID",
                      comb.fixed = FALSE,
                      comb.random = TRUE,
                      title = "Biomarker MetaAnalysis")
@@ -47,3 +47,25 @@ m.gen_results <- metagen(TE = data2_factor$AUC,
   #select(where(is.double))
 
 #data_nonzeroAUC <- data[which(data$AUC > 0),]
+
+#Set the IDs for the rows in R and calculate the within group variance
+data2_CA19$id <- as.factor(rownames(data2_CA19))
+data2_CA19_variance <- as.data.frame(apply(data2_CA19[,11], 2, function(x) tapply(x, data2_CA19$`Study ID`, var)))
+colnames(data2_CA19_variance)[1] <- "AUC_var"
+
+#Prepare the dataframe for multilevel metaanalysis with within group variance
+data2_CA19_variance[is.na(data2_CA19_variance)] = 0
+data2_CA19_variance$'Study ID' <- rownames(data2_CA19_variance)
+data2_CA19_meta <- left_join(data2_CA19, data2_CA19_variance)
+
+#Perform the multilevel metaanalysis
+full.model <- rma.mv(yi = AUC, 
+                     V = AUC_var, 
+                     slab = Title,
+                     data = data2_CA19_meta,
+                     random = ~ 1 | Title/id, 
+                     test = "t", 
+                     method = "REML")
+
+
+#Perform subgroup analyses in three-level models to assess moderators
